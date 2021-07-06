58ff3006619aa5a6b5b579d143fdaf85e036bc7b
jakub.cichorski@hotmail.com <jakub.cichorski@hotmail.com>
2021-07-06 18:27:43 +0200

    formatting

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,18 +5,18 @@ import './Map.css';
 function Map({ coords, locName, positions, zoom, scrollWheelZoom }) {
   return (
     <div className="map-container">
-    <MapContainer center={coords} zoom={zoom} scrollWheelZoom={scrollWheelZoom} >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={coords}>
-        <Popup>
-          {locName}
-        </Popup>
-      </Marker>
-      <MapPolygon positions={positions} zoom={zoom} />
-    </MapContainer>
+      <MapContainer center={coords} zoom={zoom} scrollWheelZoom={scrollWheelZoom} >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={coords}>
+          <Popup>
+            {locName}
+          </Popup>
+        </Marker>
+        <MapPolygon positions={positions} zoom={zoom} />
+      </MapContainer>
     </div>
   )
 };
